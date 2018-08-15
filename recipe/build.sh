96f103a8eb570b1992944f045e9f948da259a196
Filipe Fernandes <ocefpaf@gmail.com>
2018-08-15 13:35:42 -0300

    test OS X

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+if [ "$(uname)" == "Darwin" ]; then
+    export CC=clang
+    export CXX=clang++
+    export MACOSX_VERSION_MIN="10.9"
+    export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
+    export CMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
+    export CFLAGS="${CFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+    export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export LDFLAGS="${LDFLAGS} -lc++"
+    export LINKFLAGS="${LDFLAGS}"
+fi
+
 CFLAGS="-I${PREFIX}/include ${CFLAGS}" "${PYTHON}" -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
