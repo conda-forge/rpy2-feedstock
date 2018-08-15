c4d5be8350f599a993786979ef024eb17a16c4df
Filipe Fernandes <ocefpaf@gmail.com>
2018-08-15 13:55:40 -0300

    test OS X

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     # export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
-    export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export LDFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
     export LDFLAGS="${LDFLAGS} -lc++"
     export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib" 
     export LINKFLAGS="${LDFLAGS}"
