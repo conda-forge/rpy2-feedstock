7b469b3f0d36037ea790e9d5c9aa783add9a7044
Filipe Fernandes <ocefpaf@gmail.com>
2018-08-15 13:48:03 -0300

    test OS X

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 if [ "$(uname)" == "Darwin" ]; then
-    export CC=clang
-    export CXX=clang++
-    export MACOSX_VERSION_MIN="10.9"
-    export MACOSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-    export CMAKE_OSX_DEPLOYMENT_TARGET="${MACOSX_VERSION_MIN}"
-    export CFLAGS="${CFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
-    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
+    # export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
     export LDFLAGS="${LDFLAGS} -mmacosx-version-min=${MACOSX_VERSION_MIN}"
     export LDFLAGS="${LDFLAGS} -lc++"
+    export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib" 
     export LINKFLAGS="${LDFLAGS}"
 fi
 
