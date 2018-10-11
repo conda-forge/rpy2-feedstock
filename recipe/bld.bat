e099c97862da1cd7c37fc0476897b8cd2444beeb
Ray Donnelly <mingw.android@gmail.com>
2018-10-10 20:42:41 -0500

    Make all the msvcrts, cmd.exe is behaving weirdly with my nested braces

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,23 +8,9 @@ dlltool -d python%CONDA_PY%.def -l %CONDA_DEFAULT_ENV%\libs\libpython%CONDA_PY%.
 
 :: Create an empty library for msvcrt??? since CygwinCCompiler seems to
 :: think that linking to that is a good idea (it is not).
-if "%CONDA_PY%" == "37" (
-  ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
-else (
-  if "%CONDA_PY%" == "36" (
-    ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
-  ) else (
-    if "%CONDA_PY%" == "35" (
-      ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
-    ) else (
-      if "%CONDA_PY%" == "34" (
-        ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr120.dll.a
-      ) else (
-        ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr90.dll.a
-      )
-    )
-  )
-)
+ar cru %PREFIX%\libs\libmsvcr140.dll.a
+ar cru %PREFIX%\libs\libmsvcr120.dll.a
+ar cru %PREFIX%\libs\libmsvcr90.dll.a
 
 gendef %PREFIX%\python%CONDA_PY%.dll - > python%CONDA_PY%.def
 dlltool -d python%CONDA_PY%.def -l %PREFIX%\lib\libpython%CONDA_PY%.dll.a
@@ -34,20 +20,6 @@ if errorlevel 1 exit 1
 
 :: Cleanup the libs we made:
 del %CONDA_DEFAULT_ENV%\libs\libpython%CONDA_PY%.dll.a
-if "%CONDA_PY%" == "37" (
-  del %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
-) else (
-  if "%CONDA_PY%" == "36" (
-    del %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
-  ) else (
-    if "%CONDA_PY%" == "35" (
-      del %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
-    ) else (
-      if "%CONDA_PY%" == "34" (
-        del %CONDA_DEFAULT_ENV%\libs\libmsvcr120.dll.a
-      ) else (
-        del %CONDA_DEFAULT_ENV%\libs\libmsvcr90.dll.a
-      )
-    )
-  )
-)
+del %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
+del %CONDA_DEFAULT_ENV%\libs\libmsvcr120.dll.a
+del %CONDA_DEFAULT_ENV%\libs\libmsvcr90.dll.a
