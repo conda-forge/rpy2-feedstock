72274e591d7fe9c9ed2cd91e728a9756cdceb9ba
Isuru Fernando <isuruf@gmail.com>
2024-12-03 10:45:04 -0800

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,8 @@
 set "DISTUTILS_DEBUG=1"
+del setup.cfg
+
 "%PYTHON%" -c "import setuptools,distutils,logging;logging.basicConfig(level=logging.DEBUG);list(distutils.dist.Distribution().find_config_files())"
+
 
 "%PYTHON%" -m pip install . --no-deps -vv
 if errorlevel 1 exit 1
