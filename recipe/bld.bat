2bfae7ace1d24bc7e65270f5e7af59c853ca7e58
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2025-07-16 22:59:35 +0100

    updated v3.6.1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,8 @@
-"%PYTHON%" -m pip install . --no-deps -vv
-if errorlevel 1 exit 1
+@echo on
+
+echo [build] > rpy2-rinterface/setup.cfg
+echo compiler = mingw32 >> rpy2-rinterface/setup.cfg
+set RPY2_API_FORCE=True
+
+%PYTHON% -m pip install ./rpy2-rinterface/ ./rpy2-robjects/ . -vv -C--global-option=--verbose
+if %ERRORLEVEL% neq 0 exit 1
