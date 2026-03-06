81b45dc885f7019569137f8ef3197eb031b032e6
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2026-03-06 17:33:04 +0000

    Add RPY2_CFFI_MODE environment variable

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@
 echo [build] > rpy2-rinterface/setup.cfg
 echo compiler = mingw32 >> rpy2-rinterface/setup.cfg
 set RPY2_API_FORCE=True
+set RPY2_CFFI_MODE=API
 
 %PYTHON% -m pip install ./rpy2-rinterface/ ./rpy2-robjects/ . -vv -C--global-option=--verbose
 if %ERRORLEVEL% neq 0 exit 1
